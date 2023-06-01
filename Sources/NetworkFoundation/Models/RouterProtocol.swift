6dc74d63b161b2e687db18ba8118574972e05da0
Bilal <bilalbakhrom@gmail.com>
2023-06-01 16:45:13 +0500

    Document RouterProtocol

--- Sources/NetworkFoundation/Models/RouterProtocol.swift
+++ Sources/NetworkFoundation/Models/RouterProtocol.swift
@@ -7,21 +7,37 @@
 import Foundation
 
 public protocol RouterProtocol: URLRequestConvertible {
+    /// The HTTP method for the request.
     var method: HTTPMethod { get }
+    /// The host of the URL.
     var host: String { get }
+    /// The path component of the URL.
     var path: String { get }
+    /// The HTTP headers for the request.
     var headers: HTTPHeaders { get }
+    /// The parameters to be included in the request's body.
     var bodyParameters: Parameters? { get }
+    /// The parameters to be included in the request's URL query string.
     var queryParameters: Parameters? { get }
 }
 
 extension RouterProtocol {
+    /// The default implementation of the `headers` property, returning `nil`.
     public var headers: HTTPHeaders { return [:] }
     
+    /// The default implementation of the `bodyParameters` property, returning `nil`.
     public var bodyParameters: Parameters? { return nil }
     
+    /// The default implementation of the `queryParameters` property, returning `nil`.
     public var queryParameters: Parameters? { return nil }
     
+    
+    /// Default implementation of `asURLRequest` method.
+    ///
+    /// Creates a `URLRequest` object based on the router's properties, using the provided `URLComponentsFactory` to construct the URL and handle query parameters.
+    ///
+    /// - Throws: An error if there was an issue creating the `URLRequest`.
+    /// - Returns: A `URLRequest` object representing the router's request.
     public func asURLRequest() throws -> URLRequest {
         let urlComponentsFactory = URLComponentsFactory()
         let components = try urlComponentsFactory.create(from: host, queryItems: queryParameters)
