7281b989cfb907cda39d4a2d45f65ee3fb633c3a
Bilal <bilalbakhrom@gmail.com>
2023-12-12 13:28:19 +0500

    feat: extend settings

--- Sources/NetworkFoundation/Models/Encoder/HTTPBodyEncoderType.swift
+++ Sources/NetworkFoundation/Models/Encoder/HTTPBodyEncoderType.swift
@@ -1,5 +1,5 @@
 //
-//  EncoderType.swift
+//  HTTPBodyEncoderType.swift
 //
 //
 //  Created by Bilal Bakhrom on 2023-10-11.
@@ -8,7 +8,7 @@
 import Foundation
 
 /// An enumeration that defines different types of encoders for the `httpBody` parameter of a `URLRequest`.
-public enum EncoderType {
+public enum HTTPBodyEncoderType {
     /// Uses a custom implementation of `URLEncoder` to encode the `httpBody` parameter of the `URLRequest`.
     /// 
     /// This encoder is designed for encoding data into a URL-encoded format for the HTTP request body.
